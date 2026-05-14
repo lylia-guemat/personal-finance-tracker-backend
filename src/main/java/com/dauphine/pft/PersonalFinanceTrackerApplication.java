35818d2f957e116b0b411fadb8b617fb118eaaec
lguemat <lguemat@karavel.com>
2026-05-14 22:06:01 +0200

    connexion to supabase + dependencies

--- src/main/java/com/dauphine/pft/PersonalFinanceTrackerApplication.java
+++ src/main/java/com/dauphine/pft/PersonalFinanceTrackerApplication.java
@@ -1,9 +1,20 @@
 package com.dauphine.pft;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Personal Finance Tracker",
+				description = "Personal Finance Tracker endpoints ans apis",
+				contact = @Contact(name = "Lylia", email = "lyliaguemat2003@gmail.com"),
+				version = "1.0.0"
+		)
+)
 public class PersonalFinanceTrackerApplication {
 
 	public static void main(String[] args) {
